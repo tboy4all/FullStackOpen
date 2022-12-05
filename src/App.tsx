f14cc72c8d92ddf057fc17c4a626e6ca8b1c2b4d
tboy4all <taiyesuleiman09@gmail.com>
2022-12-05 13:37:21 +0100

    Done with excercise one

--- src/App.tsx
+++ src/App.tsx
@@ -1,104 +1,64 @@
-import React from 'react'
-import Content from './components/Content'
-// import Footer from './components/Footer'
-import Header from './components/Header'
-import Total from './components/Total'
+import React, { useState } from 'react'
 
 const App = () => {
-  // const course = 'Half Stack application development'
-  // const part1 = 'Fundamentals of React'
-  // const exercises1 = 10
-  // const part2 = 'Using props to pass data'
-  // const exercises2 = 7
-  // const part3 = 'State of a component'
-  // const exercises3 = 14
+  const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
+  ]
 
-  // const total = exercises1 + exercises2 + exercises3
+  const [selected, setSelected] = useState(0)
+  const [votes, setVotes] = useState(
+    new Array(7 + 1).join('0').split('').map(parseFloat)
+  )
 
-  // const course = 'Half Stack application development'
-  // const part1 = {
-  //   name: 'Fundamentals of React',
-  //   exercises: 10,
-  // }
-  // const part2 = {
-  //   name: 'Using props to pass data',
-  //   exercises: 7,
-  // }
-  // const part3 = {
-  //   name: 'State of a component',
-  //   exercises: 14,
-  // }
-
-  // const course = 'Half Stack application development'
-  // const parts = [
-  //   {
-  //     name: 'Fundamentals of React',
-  //     exercises: 10,
-  //   },
-  //   {
-  //     name: 'Using props to pass data',
-  //     exercises: 7,
-  //   },
-  //   {
-  //     name: 'State of a component',
-  //     exercises: 14,
-  //   },
-  // ]
-
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10,
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7,
-      },
-      {
-        name: 'State of a component',
-        exercises: 14,
-      },
-    ],
+  const getRandomQuote = () => {
+    setSelected(Math.floor(Math.random() * 6))
   }
-  const total =
-    course.parts[0].exercises +
-    course.parts[1].exercises +
-    course.parts[2].exercises
+
+  const addVote = () => {
+    setVotes((prevState) => {
+      const newVotes = [...prevState]
+      newVotes[selected] += 1
+      return [...newVotes]
+    })
+  }
+
+  const highestVote = () => {
+    return votes.reduce((cur, prev) => {
+      if (cur >= prev) {
+        return cur
+      } else {
+        return prev
+      }
+    }, 0)
+  }
 
   return (
     <>
+      <h2>Anecdote of the Day</h2>
+      {anecdotes[selected]}
       <div>
-        <h1>
-          <Header course={course.name} />
-        </h1>
-
-        <Content
-          name={course.parts[0].name}
-          exercises={course.parts[0].exercises}
-        />
-        <Content
-          name={course.parts[1].name}
-          exercises={course.parts[1].exercises}
-        />
-        <Content
-          name={course.parts[2].name}
-          exercises={course.parts[2].exercises}
-        />
-        {/* <Content name={parts[1].name} exercises={parts[1].exercises} />
-        <Content name={parts[2].name} exercises={parts[2].exercises} /> */}
-
-        {/* <p>
-        <Content part={part2} exercises={exercises2} />
-      </p>
-      <p>
-        <Content part={part3} exercises={exercises3} />
-      </p> */}
-        <p>
-          Number of exercises <Total total={total} />
-        </p>
+        <strong>has {votes[selected]} votes : </strong>
       </div>
+      <br />
+      <div>
+        <button onClick={getRandomQuote}>next anecdote</button> &nbsp;
+        <button onClick={addVote}>Vote</button>
+      </div>
+      {votes.reduce((prev, cur) => prev + cur, 0) === 0 ? (
+        ''
+      ) : (
+        <div>
+          <h2>Anecdote with most votes</h2>{' '}
+          {anecdotes[votes.indexOf(highestVote())]} <br />
+          <strong>has {highestVote()} votes</strong>
+        </div>
+      )}
     </>
   )
 }
